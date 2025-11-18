ab6fbcc5ccfdc5a7c1b515bb6122c199291f5462
Gianlucagadotti <gianlucagadotti@gmail.com>
2025-11-18 15:17:15 -0300

    Feat: #2 listagem de filmes feita

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-      <router-link to="HomeView">SPYo-Movies</router-link>
+      <router-link to="/">SPYo-Movies</router-link>
       <div>
         <ul>
           <li>
-            <router-link to="/Filmes">Filmes</router-link>
+            <router-link to="/filmes">Filmes</router-link>
           </li>
           <li>
             <router-link>Séries</router-link>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -3,22 +3,23 @@ import { ref, onMounted } from 'vue';
 import api from '@/plugins/axios';
 import { useGeneroStore } from '@/stores/generos';
 
-const genreStore = useGeneroStore();
+const generoStore = useGeneroStore();
 
 onMounted(async () => {
 
-  await genreStore.getTodosGeneros('movie');
+  await generoStore.getTodosGeneros('movie');
 
 });
 const movies = ref([]);
 
 const listaFilmes = async (generoId) => {
-  genreStore.setIdGeneroAtual(generoId);
+  generoStore.setIdGeneroAtual(generoId);
 
 
   const response = await api.get('discover/movie', {
     params: {
       with_genres: generoId,
+      with_keywords: '470',
       language: 'pt-BR',
     },
   });
@@ -35,11 +36,11 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
   <ul class="genre-list">
 
       <li
-    v-for="genero in genreStore.genres"
+    v-for="genero in generoStore.generos"
     :key="genero.id"
     @click="listaFilmes(genero.id)"
     class="genre-item"
-    :class="{ active: genero.id === genreStore.idGeneroAtual }"
+    :class="{ active: genero.id === generoStore.idGeneroAtual }"
   >
     {{ genero.name }}
   </li>
@@ -60,9 +61,9 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
   v-for="genre_id in movie.genre_ids"
   :key="genre_id"
   @click="listaFilmes(genre_id)"
-  :class="{ active: genre_id === genreStore.idGeneroAtual }"
+  :class="{ active: genre_id === generoStore.idGeneroAtual }"
 >
-  {{ genreStore.getNomeGeneros(genre_id) }}
+  {{ generoStore.getNomeGeneros(genre_id) }}
 </span>
         </p>
 
@@ -72,3 +73,97 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
 
 
 </template>
+
+<style scoped>
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
+
+.genre-item {
+  background-color: #5e5e5e;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #6e0000;
+  box-shadow: 0 0 0.5rem #fd0000;
+}
+
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+.movie-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+.movie-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
+
+.movie-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.movie-genres span {
+  background-color: #088767;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.movie-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
+}
+.active {
+  background-color: #ffffff;
+  font-weight: bolder;
+  color: #000;
+}
+
+.movie-genres span.active {
+  background-color: #abc322;
+  color: #000;
+  font-weight: bolder;
+}
+</style>
